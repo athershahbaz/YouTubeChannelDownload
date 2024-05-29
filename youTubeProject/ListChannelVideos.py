64c5db92b61c38d0314f54dad9b1412bcc517a1e
Ather <ather.shahbaz@gmail.com>
2024-05-30 01:51:00 +0500

    Get API key from file

--- youTubeProject/ListChannelVideos.py
+++ youTubeProject/ListChannelVideos.py
@@ -6,6 +6,8 @@
 
 import os
 import json
+import pandas as pd
+import openpyxl
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
@@ -28,7 +30,7 @@ def getChannelVideos(channelId, apiClient):
     print("Total videos found: ", totalResults)
 
     for item in response["items"]:
-        videoId = item["id"]["videoId"]
+        videoId = "https://www.youtube.com/watch?v=" + item["id"]["videoId"]
         publishedDate = item["snippet"]["publishedAt"]
         videoTitle = item["snippet"]["title"]
         videoDescription = item["snippet"]["description"]
@@ -42,30 +44,43 @@ def getChannelVideos(channelId, apiClient):
         #if there is a nextPageToken then retrieve results for next page.
         request = apiClient.search().list(channelId=channelId, type="video", part="snippet", maxResults=maxResults, pageToken=pageToken)
         response = request.execute()
-        
+        i = 0
         for item in response["items"]:
             #
-            videoId = item["id"]["videoId"]
+            videoId = "https://www.youtube.com/watch?v=" + item["id"]["videoId"]
             publishedDate = item["snippet"]["publishedAt"]
             videoTitle = item["snippet"]["title"]
             videoDescription = item["snippet"]["description"]
             thumbnails = item["snippet"]["thumbnails"]["high"]["url"]
             channelTitle = item["snippet"]["channelTitle"]
             allVideos.append({"videoId": videoId, "publishedDate": publishedDate, "videoTitle": videoTitle, "videoDescription": videoDescription, "thumbnails":thumbnails, "channelTitle":channelTitle})    
+        print(allVideos[i])
+        i = i + 1
+        print("Is there a next page", "nextPageToken" in list(response))
+        print("Number of items found on this page is: ",len(response["items"]))
+        input("Press Enter to proceed")
+
     
     return allVideos
 
 
 def main():
-    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey='AIzaSyB_vU6LTDoej7PcP9tRS6E0n--6rMpYS9A')
-
+    key = open("googleApi.key", "r").read().strip()
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=key)
+    
     channelHandle = input("Enter handle for chnnel")
+    fileName = "C:\\GitProjects\\archive\\" + channelHandle + "_videos.xlsx"
     
     channleId = getChannelId(channelHandle, youtube)
     
     channelVideos = getChannelVideos(channleId, youtube)
     
-    print(json.dumps(channelVideos, sort_keys=True, indent=3))
+    #print(json.dumps(channelVideos, sort_keys=True, indent=3))
+
+    df = pd.DataFrame(channelVideos)
+    print(df)
+    df.to_excel(fileName, index=False)
+
 
 if __name__ == "__main__":
     main()
